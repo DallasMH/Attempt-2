touchHLE/touchHLE

Substitute AdWhirl classes
Change-Id: Iea82d9a2ceace7877058b055fe98206dbb8f849b

src/objc/classes.rs
@@ -464,7 +464,8 @@ fn substitute_classes(
         || name.starts_with("FB") // Facebook
         || name.starts_with("Flurry")
         || name.starts_with("OpenFeint")
-        || name.starts_with("Tapjoy"))
+        || name.starts_with("Tapjoy")
+        || name.starts_with("AdWhirl"))
     {
         return None;
     }

DallasMH/Attempt-2

src/objc/classes.rs
@@ -459,10 +459,11 @@ fn substitute_classes(
     // don't support yet. This isn't "ad blocking" because ads no longer work
     // on real devices anyway :)
     if !(name.starts_with("AdMob")
+        || name.starts_with("AdWhirl")
         || name.starts_with("AltAds")
-        || name.starts_with("Mobclix")
         || name.starts_with("FB") // Facebook
         || name.starts_with("Flurry")
+        || name.starts_with("Mobclix")
         || name.starts_with("OpenFeint")
         || name.starts_with("Tapjoy"))
     {